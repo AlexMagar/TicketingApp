54d4d0f1833c90a2b459003644aa3f79eb46d974
AlexMagar <lx.mgr07@gmail.com>
2024-06-12 22:27:23 +1000

    chore: ticket page edit mode

--- app/TicketPage/[id]/page.jsx
+++ app/TicketPage/[id]/page.jsx
@@ -1,7 +1,11 @@
 import TicketForm from '@/app/(components)/TicketForm'
 import React from 'react'
 
-const TicketPage = ({params}) => {
+const TicketPage = async ({params}) => {
+
+  const EDITMODE = params.id === 'new' ? false : true
+  let updateTicketData = {}
+
   return (
     <div>
       <TicketForm />
