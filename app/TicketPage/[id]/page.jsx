ae80b84e704f1a6440276584457c1c3b4ffc102e
AlexMagar <lx.mgr07@gmail.com>
2024-06-15 18:57:16 +1000

    feat: get the ticket data and feed into updateTicketData

--- app/TicketPage/[id]/page.jsx
+++ app/TicketPage/[id]/page.jsx
@@ -1,14 +1,38 @@
 import TicketForm from '@/app/(components)/TicketForm'
 import React from 'react'
 
+const getTicketById = async (id) => {
+  try {
+    const res = await fetch(`http://localhost:3000/api/Tickets/${id}`, {
+      cache: 'no-store'
+    })
+
+    if(!res.ok){
+      throw new Error("failed to get ticket")
+    }
+    return res.json()
+  } catch (error) {
+    console.log(error)
+  }
+}
+
 const TicketPage = async ({params}) => {
 
   const EDITMODE = params.id === 'new' ? false : true
   let updateTicketData = {}
 
+  if(EDITMODE){
+    updateTicketData = await getTicketById(params.id)
+    updateTicketData = updateTicketData.foundTicket
+  }else{
+    updateTicketData = {
+      _id: 'new'
+    }
+  }
+
   return (
     <div>
-      <TicketForm />
+      <TicketForm ticket={updateTicketData}/>
     </div>
   )
 }
